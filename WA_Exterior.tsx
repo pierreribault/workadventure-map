8f7878a433183d5f41608df0972b8bbd800dc185
Loan Besson <loan.besson@hotmail.fr>
2022-06-08 16:54:25 +0200

    Test map escape

---
+++ WA_Exterior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="WA_Exterior" tilewidth="32" tileheight="32" tilecount="850" columns="25">
+ <image source="assets/WA_Exterior.png" width="800" height="1088"/>
+</tileset>
