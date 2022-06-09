6adedb2830b607ec7b2a85f4ae1262644a1f6b0c
Loan Besson <loan.besson@hotmail.fr>
2022-06-09 16:16:02 +0200

    LetsGo

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,20 @@ WA.onInit().then(() => {
         currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
     })
 
+    WA.room.onEnterLayer('popupZone').subscribe(() => {
+        currentPopup = WA.ui.openPopup("zonePopup","nique ta pute de mère",[]);
+    })
+
+    WA.room.onEnterLayer('openWallZone').subscribe(() => {
+        WA.room.hideLayer('openWall');
+    })
+
+    WA.room.onEnterLayer('closeWallZone').subscribe(() => {
+        WA.room.showLayer('openWall');
+    })
+
+    WA.room.onLeaveLayer('popupZone').subscribe(closePopUp)
+
     WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
